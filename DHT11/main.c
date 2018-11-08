50a3d8fd34d683976d0afe3476a954a6c9eba869
Anurag Chugh <lithiumhead@gmail.com>
2018-11-08 17:19:14 +0530

    clang format run, DHT11 fixes - DHT11 still work in progress

--- DHT11/main.c
+++ DHT11/main.c
@@ -1,32 +1,29 @@
-//Program Description: Read DHT11 and send to UART
-//Author: Anurag Chugh
+// Program Description: Read DHT11 and send to UART
+// Author: Anurag Chugh
 
-#define DHT11_pin       P3_5
+#define DHT11_pin P3_5
 
-#define _nop_() \
-      __asm     \
-          nop   \
-      __endasm  \
+#define _nop_() __asm nop __endasm
 
 #include "../STC15F2K60S2.H"
- 
+
 typedef unsigned char BYTE;
 typedef unsigned int WORD;
- 
+
 #define FOSC 11059200L
 #define BAUD 115200
- 
-#define NONE_PARITY     0
-#define ODD_PARITY      1
-#define EVEN_PARITY     2
-#define MARK_PARITY     3
-#define SPACE_PARITY    4
- 
+
+#define NONE_PARITY 0
+#define ODD_PARITY 1
+#define EVEN_PARITY 2
+#define MARK_PARITY 3
+#define SPACE_PARITY 4
+
 #define PARITYBIT EVEN_PARITY
- 
-#define S1_S0 0x40              //P_SW1.6
-#define S1_S1 0x80              //P_SW1.7
- 
+
+#define S1_S0 0x40 // P_SW1.6
+#define S1_S1 0x80 // P_SW1.7
+
 char busy;
 
 unsigned char DHT11_data[5];
@@ -38,11 +35,11 @@ void DHT11_init();
 unsigned short DHT11_get_byte();
 unsigned short DHT11_get_data();
 
-void uitoa(unsigned int value, char* string, int radix);
-void itoa(int value, char* string, int radix);
+void uitoa(unsigned int value, char *string, int radix);
+void itoa(int value, char *string, int radix);
 
 void main() {
-  char reading[5] = {0}; //Store Temperature/Humidity String here
+  char reading[5] = {0}; // Store Temperature/Humidity String here
 
   P0M0 = 0x00;
   P0M1 = 0x00;
@@ -60,76 +57,78 @@ void main() {
   P6M1 = 0x00;
   P7M0 = 0x00;
   P7M1 = 0x00;
- 
+
   ACC = P_SW1;
-  ACC &= ~(S1_S0 | S1_S1);    //S1_S0=0 S1_S1=0
-  P_SW1 = ACC;                //(P3.0/RxD, P3.1/TxD)
-   
-//  ACC = P_SW1;
-//  ACC &= ~(S1_S0 | S1_S1);    //S1_S0=1 S1_S1=0
-//  ACC |= S1_S0;               //(P3.6/RxD_2, P3.7/TxD_2)
-//  P_SW1 = ACC;  
-//  
-//  ACC = P_SW1;
-//  ACC &= ~(S1_S0 | S1_S1);    //S1_S0=0 S1_S1=1
-//  ACC |= S1_S1;               //(P1.6/RxD_3, P1.7/TxD_3)
-//  P_SW1 = ACC;  
- 
+  ACC &= ~(S1_S0 | S1_S1); // S1_S0=0 S1_S1=0
+  P_SW1 = ACC;             //(P3.0/RxD, P3.1/TxD)
+
+  //  ACC = P_SW1;
+  //  ACC &= ~(S1_S0 | S1_S1);    //S1_S0=1 S1_S1=0
+  //  ACC |= S1_S0;               //(P3.6/RxD_2, P3.7/TxD_2)
+  //  P_SW1 = ACC;
+  //
+  //  ACC = P_SW1;
+  //  ACC &= ~(S1_S0 | S1_S1);    //S1_S0=0 S1_S1=1
+  //  ACC |= S1_S1;               //(P1.6/RxD_3, P1.7/TxD_3)
+  //  P_SW1 = ACC;
+
 #if (PARITYBIT == NONE_PARITY)
-  SCON = 0x50;                //8-bit variable baud rate
-#elif (PARITYBIT == ODD_PARITY) || (PARITYBIT == EVEN_PARITY) || (PARITYBIT == MARK_PARITY)
-  SCON = 0xda;                //9-bit variable baud rate, parity bit is initially 1
+  SCON = 0x50; // 8-bit variable baud rate
+#elif (PARITYBIT == ODD_PARITY) || (PARITYBIT == EVEN_PARITY) ||               \
+    (PARITYBIT == MARK_PARITY)
+  SCON = 0xda; // 9-bit variable baud rate, parity bit is initially 1
 #elif (PARITYBIT == SPACE_PARITY)
-  SCON = 0xd2;                //9-bit variable baud rate, parity bit is initially 0
+  SCON = 0xd2; // 9-bit variable baud rate, parity bit is initially 0
 #endif
- 
-  T2L = (65536 - (FOSC/4/BAUD));   //Set baud rate reload value
-  T2H = (65536 - (FOSC/4/BAUD))>>8;
-  AUXR = 0x14;                //T2 is in 1T mode, and start Timer 2.
-  AUXR |= 0x01;               //Select Timer 2 as the baud rate generator of serial port 1.
-  ES = 1;                     //Enable serial port 1 interrupt.
+
+  T2L = (65536 - (FOSC / 4 / BAUD)); // Set baud rate reload value
+  T2H = (65536 - (FOSC / 4 / BAUD)) >> 8;
+  AUXR = 0x14;  // T2 is in 1T mode, and start Timer 2.
+  AUXR |= 0x01; // Select Timer 2 as the baud rate generator of serial port 1.
+  ES = 1;       // Enable serial port 1 interrupt.
   EA = 1;
- 
+
   DHT11_init();
 
   SendString("STC15L2K32S2\r\nUART1 @ 115200 bps!\r\nDHT11 @ P3.5:\r\n");
-  while(1) {
-    state = DHT11_get_byte();
-    switch(state) {
-      case 1: {
-      }
-      case 2: {
-        SendString("No Sensor Found!\r\n");
-      } break;
-      case 3: {
-        SendString("Checksum Error!\r\n");
-      } break;
-      default: {
-        SendString("Humidity: ");
-        uitoa(DHT11_data[0], reading, 10);
-        SendString(reading);
-        SendString(" % | Temperature: ");
-        uitoa(DHT11_data[2], reading, 10);
-        SendString(reading);
-        SendString(" Celsius\r\n");
-      } break;
+  while (1) {
+    unsigned char state = DHT11_get_data();
+    switch (state) {
+    case 1: {
+    }
+    case 2: {
+      SendString("No Sensor Found!\r\n");
+    } break;
+    case 3: {
+      SendString("Checksum Error!\r\n");
+    } break;
+    default: {
+      SendString("Humidity: ");
+      uitoa(DHT11_data[0], reading, 10);
+      SendString(reading);
+      SendString(" % | Temperature: ");
+      uitoa(DHT11_data[2], reading, 10);
+      SendString(reading);
+      SendString(" Celsius\r\n");
+    } break;
     };
-    { //1 second delay @11.0592MHz
+    { // 1 second delay @11.0592MHz
       unsigned char i = 43, j = 6, k = 203;
       _nop_();
       _nop_();
       do {
         do {
-          while (--k);
+          while (--k)
+            ;
         } while (--j);
       } while (--i);
     }
   };
 }
- 
+
 // UART Interrupt service routine
 void Uart(void) __interrupt(SI0_VECTOR) {
-  BYTE received_byte=0;
+  BYTE received_byte = 0;
   if (RI) {
     RI = 0;
     received_byte = SBUF;
@@ -145,42 +144,44 @@ void Uart(void) __interrupt(SI0_VECTOR) {
 
 // Send a character over UART
 void SendData(BYTE dat) {
-  while (busy);               //Waiting for the previous data to be sent
-  ACC = dat;                  //Get check bit (PSW.0)
-  if (P) {                    //Set check bit according to Parity
+  while (busy)
+    ;        // Waiting for the previous data to be sent
+  ACC = dat; // Get check bit (PSW.0)
+  if (P) {   // Set check bit according to Parity
 #if (PARITYBIT == ODD_PARITY)
-    TB8 = 0;                //Set Check Bit to 0
+    TB8 = 0; // Set Check Bit to 0
 #elif (PARITYBIT == EVEN_PARITY)
-    TB8 = 1;                //Set Check Bit to 1
+    TB8 = 1;   // Set Check Bit to 1
 #endif
   } else {
 #if (PARITYBIT == ODD_PARITY)
-    TB8 = 1;                //Set Check Bit to 1
+    TB8 = 1; // Set Check Bit to 1
 #elif (PARITYBIT == EVEN_PARITY)
-    TB8 = 0;                //Set Check Bit to 0
+    TB8 = 0;   // Set Check Bit to 0
 #endif
   }
   busy = 1;
-  SBUF = ACC;                 //Write data to the UART data register
+  SBUF = ACC; // Write data to the UART data register
 }
 
 // Send a whole string over UART
 void SendString(char *s) {
-  while (*s) {                //Detect string end character '\0'
-    SendData(*s++);         //Send current character
+  while (*s) {      // Detect string end character '\0'
+    SendData(*s++); // Send current character
   }
 }
 
 // DHT11 Data pin initialization
 void DHT11_init() {
   DHT11_pin = 1;
-  { //1 second delay @11.0592MHz
+  { // 1 second delay @11.0592MHz
     unsigned char i = 43, j = 6, k = 203;
     _nop_();
     _nop_();
     do {
       do {
-        while (--k);
+        while (--k)
+          ;
       } while (--j);
     } while (--i);
   }
@@ -190,25 +191,27 @@ void DHT11_init() {
 unsigned short DHT11_get_byte() {
   unsigned short s = 0;
   unsigned short value = 0;
-  for(s = 0; s < 8; s += 1)
-  {
+  for (s = 0; s < 8; s += 1) {
     value <<= 1;
-    while((DHT11_pin == 0));
-    { //30 us delay @11.0592MHz
+    while ((DHT11_pin == 0))
+      ;
+    { // 30 us delay @11.0592MHz
       unsigned char i = 80;
       _nop_();
       _nop_();
-      while (--i);
+      while (--i)
+        ;
     }
-    if(DHT11_pin == 1) {
-        value |= 1;
+    if (DHT11_pin == 1) {
+      value |= 1;
     }
-    while(DHT11_pin == 1);
+    while (DHT11_pin == 1)
+      ;
   }
   return value;
 }
 
-//Get a complete reading from DHT11
+// Get a complete reading from DHT11
 unsigned short DHT11_get_data() {
   __bit chk;
   unsigned short s = 0;
@@ -216,68 +219,71 @@ unsigned short DHT11_get_data() {
 
   DHT11_pin = 1;
   DHT11_pin = 0;
-  { //18 ms delay @11.0592MHz
+  { // 18 ms delay @11.0592MHz
     unsigned char i = 1, j = 194, k = 160;
     do {
       do {
-        while (--k);
+        while (--k)
+          ;
       } while (--j);
     } while (--i);
   }
   DHT11_pin = 1;
-  { //26 us delay @11.0592MHz
+  { // 26 us delay @11.0592MHz
     unsigned char i = 69;
     _nop_();
     _nop_();
-    while (--i);
+    while (--i)
+      ;
   }
 
   chk = DHT11_pin;
 
-  if(chk) {
+  if (chk) {
     return 1;
   }
-  { //80 us delay @11.0592MHz
+  { // 80 us delay @11.0592MHz
     unsigned char i = 1, j = 217;
     _nop_();
     do {
-      while (--j);
+      while (--j)
+        ;
     } while (--i);
   }
 
   chk = DHT11_pin;
-  if(!chk) {
+  if (!chk) {
     return 2;
   }
-  { //80 us delay @11.0592MHz
+  { // 80 us delay @11.0592MHz
     unsigned char i = 1, j = 217;
     _nop_();
     do {
-      while (--j);
+      while (--j)
+        ;
     } while (--i);
   }
 
-  for(s = 0; s <= 4; s += 1) {
-     DHT11_data[s] = DHT11_get_byte();
+  for (s = 0; s <= 4; s += 1) {
+    DHT11_data[s] = DHT11_get_byte();
   }
 
   DHT11_pin = 1;
 
-  for(s = 0; s < 4; s += 1) {
-     check_sum += DHT11_data[s];
+  for (s = 0; s < 4; s += 1) {
+    check_sum += DHT11_data[s];
   }
 
-  if(check_sum != DHT11_data[4]) {
+  if (check_sum != DHT11_data[4]) {
     return 3;
   } else {
     return 0;
   }
 }
 
+#define NUMBER_OF_DIGITS 16 // space for NUMBER_OF_DIGITS + '\0'
 
-#define NUMBER_OF_DIGITS 16   // space for NUMBER_OF_DIGITS + '\0'
-
-void uitoa(unsigned int value, char* string, int radix) {
+void uitoa(unsigned int value, char *string, int radix) {
   unsigned char index, i;
 
   index = NUMBER_OF_DIGITS;
@@ -285,23 +291,23 @@ void uitoa(unsigned int value, char* string, int radix) {
 
   do {
     string[--index] = '0' + (value % radix);
-    if ( string[index] > '9') string[index] += 'A' - ':';   // continue with A, B,..
+    if (string[index] > '9')
+      string[index] += 'A' - ':'; // continue with A, B,..
     value /= radix;
   } while (value != 0);
 
   do {
     string[i++] = string[index++];
-  } while ( index < NUMBER_OF_DIGITS );
+  } while (index < NUMBER_OF_DIGITS);
 
   string[i] = 0; // string terminator
 }
 
-void itoa(int value, char* string, int radix) {
+void itoa(int value, char *string, int radix) {
   if (value < 0 && radix == 10) {
     *string++ = '-';
     uitoa(-value, string, radix);
-  }
-  else {
+  } else {
     uitoa(value, string, radix);
   }
 }
--- UART_Echo/main.c
+++ UART_Echo/main.c
@@ -1,30 +1,30 @@
-//Program Description:  Emulate a simple integer calculator over a Serial Link
-//Author: Anurag Chugh
+// Program Description:  Emulate a simple integer calculator over a Serial Link
+// Author: Anurag Chugh
 
 #include "../STC15F2K60S2.H"
- 
+
 typedef unsigned char BYTE;
 typedef unsigned int WORD;
- 
+
 #define FOSC 11059200L
 #define BAUD 115200
- 
-#define NONE_PARITY     0
-#define ODD_PARITY      1
-#define EVEN_PARITY     2
-#define MARK_PARITY     3
-#define SPACE_PARITY    4
- 
+
+#define NONE_PARITY 0
+#define ODD_PARITY 1
+#define EVEN_PARITY 2
+#define MARK_PARITY 3
+#define SPACE_PARITY 4
+
 #define PARITYBIT EVEN_PARITY
- 
-#define S1_S0 0x40              //P_SW1.6
-#define S1_S1 0x80              //P_SW1.7
- 
+
+#define S1_S0 0x40 // P_SW1.6
+#define S1_S1 0x80 // P_SW1.7
+
 char busy;
- 
+
 void SendData(BYTE dat);
 void SendString(char *s);
- 
+
 void main() {
   P0M0 = 0x00;
   P0M1 = 0x00;
@@ -42,43 +42,45 @@ void main() {
   P6M1 = 0x00;
   P7M0 = 0x00;
   P7M1 = 0x00;
- 
+
   ACC = P_SW1;
-  ACC &= ~(S1_S0 | S1_S1);    //S1_S0=0 S1_S1=0
-  P_SW1 = ACC;                //(P3.0/RxD, P3.1/TxD)
-   
-//  ACC = P_SW1;
-//  ACC &= ~(S1_S0 | S1_S1);    //S1_S0=1 S1_S1=0
-//  ACC |= S1_S0;               //(P3.6/RxD_2, P3.7/TxD_2)
-//  P_SW1 = ACC;  
-//  
-//  ACC = P_SW1;
-//  ACC &= ~(S1_S0 | S1_S1);    //S1_S0=0 S1_S1=1
-//  ACC |= S1_S1;               //(P1.6/RxD_3, P1.7/TxD_3)
-//  P_SW1 = ACC;  
- 
+  ACC &= ~(S1_S0 | S1_S1); // S1_S0=0 S1_S1=0
+  P_SW1 = ACC;             //(P3.0/RxD, P3.1/TxD)
+
+  //  ACC = P_SW1;
+  //  ACC &= ~(S1_S0 | S1_S1);    //S1_S0=1 S1_S1=0
+  //  ACC |= S1_S0;               //(P3.6/RxD_2, P3.7/TxD_2)
+  //  P_SW1 = ACC;
+  //
+  //  ACC = P_SW1;
+  //  ACC &= ~(S1_S0 | S1_S1);    //S1_S0=0 S1_S1=1
+  //  ACC |= S1_S1;               //(P1.6/RxD_3, P1.7/TxD_3)
+  //  P_SW1 = ACC;
+
 #if (PARITYBIT == NONE_PARITY)
-  SCON = 0x50;                //8-bit variable baud rate
-#elif (PARITYBIT == ODD_PARITY) || (PARITYBIT == EVEN_PARITY) || (PARITYBIT == MARK_PARITY)
-  SCON = 0xda;                //9-bit variable baud rate, parity bit is initially 1
+  SCON = 0x50; // 8-bit variable baud rate
+#elif (PARITYBIT == ODD_PARITY) || (PARITYBIT == EVEN_PARITY) ||               \
+    (PARITYBIT == MARK_PARITY)
+  SCON = 0xda; // 9-bit variable baud rate, parity bit is initially 1
 #elif (PARITYBIT == SPACE_PARITY)
-  SCON = 0xd2;                //9-bit variable baud rate, parity bit is initially 0
+  SCON = 0xd2; // 9-bit variable baud rate, parity bit is initially 0
 #endif
- 
-  T2L = (65536 - (FOSC/4/BAUD));   //Set baud rate reload value
-  T2H = (65536 - (FOSC/4/BAUD))>>8;
-  AUXR = 0x14;                //T2 is in 1T mode, and start Timer 2.
-  AUXR |= 0x01;               //Select Timer 2 as the baud rate generator of serial port 1.
-  ES = 1;                     //Enable serial port 1 interrupt.
+
+  T2L = (65536 - (FOSC / 4 / BAUD)); // Set baud rate reload value
+  T2H = (65536 - (FOSC / 4 / BAUD)) >> 8;
+  AUXR = 0x14;  // T2 is in 1T mode, and start Timer 2.
+  AUXR |= 0x01; // Select Timer 2 as the baud rate generator of serial port 1.
+  ES = 1;       // Enable serial port 1 interrupt.
   EA = 1;
- 
+
   SendString("STC15L2K32S2\r\nUART1 Test 115200 bps!\r\n");
-  while(1);
+  while (1)
+    ;
 }
- 
+
 // UART Interrupt service routine
 void Uart(void) __interrupt(SI0_VECTOR) {
-  BYTE received_byte=0;
+  BYTE received_byte = 0;
   if (RI) {
     RI = 0;
     received_byte = SBUF;
@@ -91,32 +93,32 @@ void Uart(void) __interrupt(SI0_VECTOR) {
     busy = 0;
   }
 }
- 
+
 // Send a character over UART
-void SendData(BYTE dat)
-{
-  while (busy);               //Waiting for the previous data to be sent
-  ACC = dat;                  //Get check bit (PSW.0)
-  if (P) {                      //Set check bit according to Parity
+void SendData(BYTE dat) {
+  while (busy)
+    ;        // Waiting for the previous data to be sent
+  ACC = dat; // Get check bit (PSW.0)
+  if (P) {   // Set check bit according to Parity
 #if (PARITYBIT == ODD_PARITY)
-    TB8 = 0;                //Set Check Bit to 0
+    TB8 = 0; // Set Check Bit to 0
 #elif (PARITYBIT == EVEN_PARITY)
-    TB8 = 1;                //Set Check Bit to 1
+    TB8 = 1;   // Set Check Bit to 1
 #endif
   } else {
 #if (PARITYBIT == ODD_PARITY)
-    TB8 = 1;                //Set Check Bit to 1
+    TB8 = 1; // Set Check Bit to 1
 #elif (PARITYBIT == EVEN_PARITY)
-    TB8 = 0;                //Set Check Bit to 0
+    TB8 = 0;   // Set Check Bit to 0
 #endif
   }
   busy = 1;
-  SBUF = ACC;                 //Write data to the UART data register
+  SBUF = ACC; // Write data to the UART data register
 }
 
 // Send a whole string over UART
 void SendString(char *s) {
-  while (*s) {                  //Detect string end character '\0'
-    SendData(*s++);         //Send current character
+  while (*s) {      // Detect string end character '\0'
+    SendData(*s++); // Send current character
   }
 }
